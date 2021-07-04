dc84fdef2ed37d25f3e4eb1ec57b2393438eee37
Pawel Stanecki <p.stanecki93@gmail.com>
2021-07-04 14:18:11 +0200

    Added ts and convert deleteAction button to tsx

---
+++ src/features/Stations/Form/DeleteAction.tsx
@@ -0,0 +1,47 @@
+import { MouseEvent } from "react";
+import PropTypes from "prop-types";
+import { useSelector } from "react-redux";
+import { makeStyles } from "@material-ui/styles";
+import DeleteForeverOutlinedIcon from "@material-ui/icons/DeleteForeverOutlined";
+import { selectStationLoading } from "../../../store/weatherStations/selectors";
+import Loading from "../../../components/Loading";
+
+const useStyles = makeStyles((theme) => ({
+  buttonWrapper: {
+    color: "#333",
+    transition: "color .15s ease-in",
+
+    "&:hover": {
+      color: "red",
+      cursor: "pointer",
+    },
+  },
+  loading: {
+    width: "20px !important",
+    height: "20px !important",
+    color: "red",
+  },
+}));
+
+type Props = {
+  action: (event: MouseEvent) => void;
+};
+
+const DeleteAction = ({ action }: Props) => {
+  const classes = useStyles();
+  const loading = useSelector(selectStationLoading);
+
+  return loading ? (
+    <Loading color="red" />
+  ) : (
+    <div className={classes.buttonWrapper}>
+      <DeleteForeverOutlinedIcon data-testid="delete-button" onClick={action} />
+    </div>
+  );
+};
+
+DeleteAction.propTypes = {
+  action: PropTypes.func,
+};
+
+export default DeleteAction;
